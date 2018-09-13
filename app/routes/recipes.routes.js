dfc781aa5978eab4b784174498d5d973bc5f25f2
MikaelFeher <mikael.feher@gmail.com>
2018-09-14 00:42:13 +0200

    Adds route to find specific recipes by (partial)name...

--- app/routes/recipes.routes.js
+++ app/routes/recipes.routes.js
@@ -7,17 +7,25 @@ module.exports = routes => {
     let recipes = await Recipe.find().lean().exec();
     if(!recipes.length) return res.json({ error: 'No recipes added yet...' });
 
-    res.json(recipes);
+    return res.json(recipes);
   });
 
   // Add a new recipe
-  routes.post('/add-recipe', async (req, res) => {
+  routes.post('/admin/add-new-recipe', async (req, res) => {
     const exists = await Recipe.find({ name: req.body.name }).lean().exec();
     if (exists.length) return res.json({ error: 'A recipe with that name already exists!' });
 
     return new Recipe(req.body)
       .save()
       .then(object => res.json(object))
+  })
+
+  // Find a recipe by name, makes partial matches...
+  routes.get('/find-by-name/:name', async (req, res) => {
+    const recipe = await Recipe.find({ name: { "$regex": req.params.name, "$options": "i"} }).lean().exec();
+    if (!recipe.length) return res.json({ error: 'No recipe with that name exists!' });
+    
+    return res.json(recipe);
   })
   
   return routes
