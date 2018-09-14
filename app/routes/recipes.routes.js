913457ee9572bd288e9335141baa5eb3d299adac
MikaelFeher <mikael.feher@gmail.com>
2018-09-14 13:48:31 +0200

    Adds route to delete a recipe...

--- app/routes/recipes.routes.js
+++ app/routes/recipes.routes.js
@@ -17,33 +17,39 @@ module.exports = routes => {
     if (!recipe.length) return res.json({ error: 'No recipe with that name exists!' });
     
     return res.json(recipe);
-  })
+  });
   
   /***** ADMIN ROUTES *****/
   // Add a new recipe
-  routes.post('/admin/add-new-recipe', async (req, res) => {
+  routes.post('/admin/recipe/add-new-recipe', async (req, res) => {
     const exists = await Recipe.find({ name: req.body.name }).lean().exec();
     if (exists.length) return res.json({ error: 'A recipe with that name already exists!' });
     
     return new Recipe(req.body)
       .save()
       .then(object => res.json(object))
-  })
+  });
   
   // Edit a recipe
-  routes.put('/admin/edit/:name', async (req, res) => {
+  routes.put('/admin/recipe/edit-recipe/:name', async (req, res) => {
     const recipes = await Recipe.find({ name: req.params.name }).lean().exec();
     const recipeToUpdate = recipes[0];
     if (!recipeToUpdate) return res.json({ error: 'No recipe with that name exists!' });
 
     const updatedRecipe = Object.assign({}, recipeToUpdate, req.body);
     
-    return Recipe.findByIdAndUpdate(recipeToUpdate._id, updatedRecipe, { new: true })
+    return Recipe.findOneAndUpdate({ _id: recipeToUpdate._id }, updatedRecipe, { new: true })
       .then(object => res.json(object));
-  })
-  
-  // TODO:
+  });
+ 
   // Delete a recipe
-  
+  routes.delete('/admin/recipe/delete-recipe/:name', async (req, res) => {
+    const recipes = await Recipe.find({ name: req.params.name }).lean().exec();
+    const recipeToDelete = recipes[0];
+
+    return Recipe.findOneAndRemove({ _id: recipeToDelete._id }).exec()
+      .then(res.json(`${recipeToDelete.name} was successfully deleted.`));
+  });
+
   return routes
 }
