6640f3225d939b03732a8b92f51fda753554e6eb
MikaelFeher <mikael.feher@gmail.com>
2018-10-01 14:03:35 +0200

    Updates ingredient routes.

--- app/routes/ingredients.routes.js
+++ app/routes/ingredients.routes.js
@@ -3,15 +3,16 @@ const ingredients = require('../json/livsmedelsdata.json');
 module.exports = routes => {
   
   // Get the first 5 ingredients...
-  routes.get('/', (req, res) => {
+  routes.get('/five', (req, res) => {
     res.json(ingredients.slice(0, 5));
   });
 
   // Get all ingredients
-  routes.get('/all', (req, res) => {
+  routes.get('/', (req, res) => {
     res.json(ingredients);
   });
   
+  // TODO: After the ingredients model is implemented this needs to be changed to take :id instead of :name...
   // Find ingredients by name...
   routes.get('/find-by-name/:name', (req, res) => {
     let name = req.params.name.toLowerCase();
