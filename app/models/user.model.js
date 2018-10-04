8fd0bcf9e3f78d692ba5ffc6b7a64e78df55d64d
MikaelFeher <mikael.feher@gmail.com>
2018-10-04 23:49:05 +0200

    Adds user mongoose model.

---
+++ app/models/user.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    index: { 
+      unique: true 
+    }
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('User', schema);
